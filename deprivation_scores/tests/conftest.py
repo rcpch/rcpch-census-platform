347b0227c53c0d71fd8ffa02c84bec7edc6600a3
eatyourpeas <simon@eatyourpeas.co.uk>
2023-12-10 16:23:24 +0000

    fix double seeding of pytest django db

--- deprivation_scores/tests/conftest.py
+++ deprivation_scores/tests/conftest.py
@@ -11,17 +11,18 @@ from django.apps import apps
 def seed_data_fixture(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock():
         # Don't repeat seed
-        # LSOA = apps.get_model("deprivation_scores", "LSOA")
+        LSOA = apps.get_model("deprivation_scores", "LSOA")
         ScottishIndexMultipleDeprivation = apps.get_model("deprivation_scores", "ScottishIndexMultipleDeprivation")
-        # if not LSOA.objects.exists():
-        call_command("seed", mode="add_organisational_areas")
-        call_command("seed", mode="add_english_imds")
-        call_command("seed", mode="add_welsh_imds")
+        EnglishIndexMultipleDeprivation = apps.get_model("deprivation_scores", "EnglishIndexMultipleDeprivation")
+        WelshIndexMultipleDeprivation = apps.get_model("deprivation_scores", "WelshIndexMultipleDeprivation")
+        if not LSOA.objects.exists():
+            call_command("seed", mode="add_organisational_areas")
+        if not EnglishIndexMultipleDeprivation.objects.exists():
+            call_command("seed", mode="add_english_imds")
+        if not WelshIndexMultipleDeprivation.objects.exists():
+            call_command("seed", mode="add_welsh_imds")
         if not ScottishIndexMultipleDeprivation.objects.exists():
             call_command("seed", mode="add_scottish_imds")
-        else:
-            print("Test database already seeded. Skipping...")
-
 
 @pytest.fixture(scope="function")
 def api_client() -> APIClient:
