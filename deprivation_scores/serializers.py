2455d30ff81c3c7b47bd464b137e301c128e4ea5
eatyourpeas <simon@eatyourpeas.co.uk>
2025-05-03 09:15:07 +0100

    serializer refactor nest lsoa and local authority

--- deprivation_scores/serializers.py
+++ deprivation_scores/serializers.py
@@ -496,9 +496,20 @@ class NorthernIrelandIndexMultipleDeprivationSerializer(
     def get_type(self, obj) -> str:
         return "English"
 
-class PopulationDensitySerializer(serializers.Serializer):
-    
+class NestedLSOASerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LSOA
+        fields = [
+            "lsoa_code",
+            "lsoa_name",
+        ]
+
+
+class PopulationDensitySerializer(serializers.ModelSerializer):
+    lsoa = NestedLSOASerializer(read_only=True)
+    local_authority = LocalAuthorityDistrictSerializer(read_only=True)
 
     class Meta:
         model = PopulationDensity
         fields ="__all__"
+        depth = 1
