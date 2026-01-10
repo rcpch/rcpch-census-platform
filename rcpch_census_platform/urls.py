9ec909b620aa9d795fb6eff208d2564593b19e18
eatyourpeas <simon@eatyourpeas.co.uk>
2026-01-10 15:58:46 +0000

    looking for /health but mccavity is not there

--- rcpch_census_platform/urls.py
+++ rcpch_census_platform/urls.py
@@ -1,10 +1,15 @@
 """rcpch_census_platform URL Configuration
 """
-from django.urls import include, path
 from django.contrib import admin
+from django.http import JsonResponse
+from django.urls import include, path
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
 
 
 urlpatterns = [
+    path("health/", health_check, name="health_check"),
     path("admin/", admin.site.urls),
     path("", include("deprivation_scores.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
